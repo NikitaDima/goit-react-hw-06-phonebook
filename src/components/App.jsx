8ca3e56fa68ffdb5f6a3e589da006f20e8b9db6a
NikitaDima <onekill13@gmail.com>
2023-05-31 00:40:01 +0300

    add style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import ContactList from './contactList/ContactList';
 const App = () => {
   return (
     <>
-      <div>
+      <div style={{ maxWidth: '500px', margin: '0 auto', padding: '20px' }}>
         <h1>Phonebook</h1>
         <ContactForm />
         <h2>Contacts</h2>
