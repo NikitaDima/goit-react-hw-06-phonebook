3238c6c2d55e18f7e67a71a75de4dbf952950f60
NikitaDima <onekill13@gmail.com>
2023-05-31 12:56:37 +0300

    storage bug fix

--- src/redux/store/Store.jsx
+++ src/redux/store/Store.jsx
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { SliceContacts, SliceFilter } from '../Slices/Slices';
-import { persistReducer, persistStore } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from '@reduxjs/toolkit';
 
@@ -18,6 +27,13 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
 });
 
 export const persistor = persistStore(store);
